4345f6df516dc19b18619633b70adad892659f5d
Patrick Cremin <patrickfxcremin@gmail.com>
2020-05-22 19:12:49 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
-  puts "Hi! I'm HAL, what's your name?"
+  return "Hi! I'm HAL, what's your name?"
   name = gets.strip
   greeting(name)
 end
